0743f713b80ab2469d0cdc51241f06bae32ffd39
root <root@hermes.(none)>
2012-11-13 11:53:32 +0100

    Some updates to compiling and linking for c/cpp

--- cpp-objects.mk
+++ cpp-objects.mk
@@ -6,27 +6,26 @@
 
 -include $(OBJECTS:.o=.d)												# Include all dependency information for all objects. (Prefixing - means if they exist).
 
+NEW_OBJECTS = $(addprefix $(BUILD_DIR),$(OBJECTS))
+
 ifeq ($(V), 3)
 qd=
 else
 qd=@
 endif
 
-%.o : %.cpp
-ifeq ($(RBUILD_VERBOSE_CMD), 0) 											# Pretty print on successful compile, but still display errors when they occur.
-	$(Q)$(PRETTY) --rbuild "CXX" $(MODULE_NAME) $*.o
-endif																	# Full verbose output for debugging (make V=1).
-	$(Q)$(CXX) -MD -MP $(CXXFLAGS) $(CFLAGS) $*.cpp -o $*.o
+$(BUILD_DIR)%.o: $(BUILD_BASE)%.cpp
+ifeq ($(DBUILD_VERBOSE_CMD), 0)
+	$(Q)$(PRETTY) --dbuild "CPP" $(MODULE_NAME) $(notdir $@)
+endif
+	@mkdir -p $(dir $@)
+	$(Q)$(CXX) -MD -MP $(CXXFLAGS) $(CFLAGS) $< -o $@
 	$(POST_CC)
 
-ifeq ($(RBUILD_VERBOSE_DEPS), 1)
-#	$(Q)$(PRETTY) --rbuild "DEP" $(MODULE_NAME) $*.d
+%.o : %.cpp
+ifeq ($(DBUILD_VERBOSE_CMD), 0)
+	$(Q)$(PRETTY) --dbuild "CPP" $(MODULE_NAME) $(notdir $@)
 endif
-# http://scottmcpeak.com/autodepend/autodepend.html						# Autogenerate Dependencies for object.
-#	$(qd)$(CXX) -MM $(CXXFLAGS) $(CFLAGS) $*.cpp -MF $*.d
-#																		# Fix the GCC dep file output format.
-#	$(qd)mv -f $*.d $*.d.tmp
-#	$(qd)sed -e 's|.*:|$*.o:|' < $*.d.tmp > $*.d
-#	$(qd)sed -e 's/.*://' -e 's/\\$$//' < $*.d.tmp | fmt -1 | \
-#	 sed -e 's/^ *//' -e 's/$$/:/' >> $*.d
-#	$(qd)rm -f $*.d.tmp
+	@mkdir -p $(dir $@)
+	$(Q)$(CXX) -MD -MP $(CXXFLAGS) $(CFLAGS) $< -o $@
+	$(POST_CC)
