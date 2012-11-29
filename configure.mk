f58b9d98d6d684b578774a1b239d977937626eca
James Walmsley <jwalmsley@riegl.com>
2012-11-29 09:50:23 +0100

    Small fix to configure targets.

--- configure.mk
+++ configure.mk
@@ -9,8 +9,10 @@
 #	This provides a nice way to call those.
 #
 $(CONFIGURE_LIST:%=%.configure): %: %.pre
+
+
 	@cd  $(@:%.configure=%) && ./configure $(CONFIG_OPTIONS) CC=$(TOOLCHAIN)gcc CXX=$(TOOLCHAIN)c++ LD=$(TOOLCHAIN)ld AR=$(TOOLCHAIN)ar $(PIPE_OPTIONS)
-	$(Q)$(MAKE) -s $(MAKE_FLAGS) DBUILD_SPLASHED=1 $(CONFIG_OPTIONS) $@.post
+	$(Q)$(MAKE) -s $(MAKE_FLAGS) DBUILD_SPLASHED=1 CONFIG_OPTIONS="$(CONFIG_OPTIONS)" $@.post
 
 configure: $(CONFIGURE_LIST:%=%.configure)
 
