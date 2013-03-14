c192aab625beb959afa2efc2c6801c4e87145759
Andreas Friedl <afriedl@riegl.com>
2013-03-14 08:52:01 +0100

    fix special character handling in makedeps.sh

--- makedeps.sh
+++ makedeps.sh
@@ -3,8 +3,8 @@
 depsdir() {
 	echo -n "$1.stamp: "; shift
 	for d; do
-		( cd "$d"; git ls-files | sed "s,^,$d/,"; )
-	done | sort | sed 'x; s/ /\\ /g; /./ { s,$, ,; }; s,$,\\,; $ { p; x; }' | sed 's/#/\\#/' 
+		( cd "$d"; git ls-files | grep -v '[\"#]' | sed "s,^,$d/,"; )
+	done | sort | sed 'x; s/ /\\ /g; /./ { s,$, ,; }; s,$,\\,; $ { p; x; }' 
 	echo
 }
 
