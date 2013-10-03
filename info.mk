7dc3f0a48aae428f614835ab033cd17475d537a0
Harald Becker <hbecker@riegl.com>
2013-10-03 14:32:12 +0200

    [KCONFIG] Added support for Kconfig.

--- info.mk
+++ info.mk
@@ -1,2 +1,12 @@
+
+info.config:
+	@echo "CONFIG_  : " $(CONFIG_)
+	@echo "TOOLCHAIN: " $($(CONFIG_)TOOLCHAIN)
+
+info.toolchain:
+	@echo "TOOLCHAIN: " $(TOOLCHAIN)
+	@echo "CC       : " $(CC)
+	@echo "CXX      : " $(CXX) 
+
 info.subdirs:
 	@echo $(SUBDIRS)
